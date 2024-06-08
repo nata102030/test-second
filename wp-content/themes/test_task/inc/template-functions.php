8a2f564940feb6fdcdfb913226d85ce828b1648f
nata102030 <aldanata102030@gmail.com>
2024-06-08 23:03:06 +0300

    [ADDED] block top-page-banner

--- wp-content/themes/test_task/inc/template-functions.php
+++ wp-content/themes/test_task/inc/template-functions.php
@@ -35,3 +35,79 @@ function test_task_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'test_task_pingback_header' );
+
+
+
+/**
+ * Gutenberg category
+ */
+function add_custom_block_categories($block_categories, $editor_context)
+{
+	if (!empty($editor_context->post)) {
+		array_push(
+			$block_categories,
+			array(
+				'slug'  => 'images-modules',
+				'title' => __('Images-Module', 'test_task'),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'text-images-modules',
+				'title' => __('Images/Text module', 'test_task'),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'text-modules',
+				'title' => __('Text module', 'test_task'),
+				'icon'  => null,
+			),
+			array(
+				'slug'  => 'special',
+				'title' => __('Special', 'test_task'),
+				'icon'  => null,
+			),
+		);
+	}
+	return $block_categories;
+}
+add_filter('block_categories_all', 'add_custom_block_categories', 10, 2);
+
+/**
+ * Gutenberg blocks
+ */
+add_action('init', 'register_acf_blocks');
+function register_acf_blocks()
+{
+	register_block_type(get_template_directory() . '/template-parts/blocks/text-images/top-page-banner');
+	
+}
+
+/**
+ * remove Gutenberg Block
+ */
+add_filter('allowed_block_types_all', 'allowed_block_types', 25, 2);
+function allowed_block_types($allowed_blocks, $editor_context)
+{
+
+	if ('post' === $editor_context->post->post_type) {
+        return array(
+            'core/paragraph',
+            'core/image',
+            'core/gallery',
+            'core/heading',
+            'core/list',
+            'core/list-item',
+            'core/video',
+            'core/embed',
+            'core/spacer',
+            'core/buttons',
+            'core/separator',
+            'acf/top-page-banner',
+			
+        );
+    }
+
+	return array(
+		'acf/top-page-banner',
+	);
+}
---
+++ wp-content/themes/test_task/template-parts/blocks/text-images/top-page-banner/top-page-banner.php
@@ -0,0 +1,70 @@
+<?php
+if (isset($block['data']['gutenberg_preview_image']) && $is_preview) {
+    echo '<img src="' . get_stylesheet_directory_uri() . '/template-parts/blocks/text-images/top-page-banner/top-page-banner.jpg" style="max-width:100%; height:auto;">';
+}
+
+if (!$is_preview) {
+    $id = 'top-page-banner-' . $block['id'];
+    if (!empty($block['anchor'])) {
+        $id = $block['anchor'];
+    }
+
+    if (!empty($block['anchor'])) {
+        $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+    }
+
+    $class_name = 'top-page-banner';
+    if (!empty($block['className'])) {
+        $class_name .= ' ' . $block['className'];
+    }
+    if (!empty($block['align'])) {
+        $class_name .= ' align' . $block['align'];
+    }
+
+    $title = get_field('title');
+    $text = get_field('text');
+    $image = get_field('image');
+    $button = get_field('button');
+
+
+    ?>
+
+    <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($class_name); ?>">
+
+   
+        
+
+          <div class="top-page-banner_wrap"> 
+
+            <div class="wrap-text">
+                <?php if ($title): ?>
+                    <h1 class="top-page-banner-title">
+                        <?php echo $title; ?>
+                    </h1>
+                <?php endif; ?>
+                <?php if ($text): ?>
+                    <div class="top-page-banner-text">
+                        <?php echo $text; ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($button): ?>
+                    <a href="<?php echo $button['url']; ?>" class="btn">
+                        <?php echo $button['title']; ?>
+                    </a>
+                <?php endif; ?>
+            </div>
+
+            <?php if ($image): ?>
+                <div class="wrap-image">
+                    <?php echo wp_get_attachment_image($image['id'], 'large'); ?>
+                </div>
+            <?php endif; ?>
+
+         </div> 
+
+       
+
+
+    </section>
+<?php } ?>
